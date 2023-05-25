eada2085780a670b7fcdb0f4712530c8591f86d0
Flower_Merlin <80883724+Nik-winner@users.noreply.github.com>
2023-05-25 15:55:29 +0600

    Add files via upload

---
+++ routes/signInRouter.js
@@ -0,0 +1,6 @@
+const exp = require("express");
+const signInController = require("../controllers/signInController.js");
+const signInRouter = exp.Router();
+
+signInRouter.use("/", signInController.signIn);
+module.exports = signInRouter;
