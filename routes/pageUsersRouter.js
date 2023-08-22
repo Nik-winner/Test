fcb6cd1aead219c674e169508ba1d7fb783d7687
Flower_Merlin <80883724+Nik-winner@users.noreply.github.com>
2023-08-22 15:06:11 +0600

    Delete pageUsersRouter.js

--- routes/pageUsersRouter.js
+++
@@ -1,6 +0,0 @@
-const exp = require("express");
-const pageUsersController = require("../controllers/pageUsersController.js");
-const pageUsers = exp.Router();
-
-pageUsers.use("/", pageUsersController.pageUsers);
-module.exports = pageUsers;
