debcd0609f71705c781dd22694def728ba12b1e2
Flower_Merlin <80883724+Nik-winner@users.noreply.github.com>
2023-12-21 16:02:28 +0600

    Add files via upload

--- public/JS/attendance.js
+++ public/JS/attendance.js
@@ -2,16 +2,37 @@ let attends = document.querySelectorAll(".attend");
 
 window.onload = function(){
     for(let attend of attends){
-        let check = attend.children
-        console.log(check)
-        if(attend.children.innerHTML == 0){
-            attend.classList.toggle("attend_check0")
-        }else if(attend.children.innerHTML == 1){
-            attend.classList.toggle("attend_check1")
-        }else if(attend.children.innerHTML == 2){
-            attend.classList.toggle("attend_check2")
-        }else if(attend.children.innerHTML == 3){
-            attend.classList.toggle("attend_check3")
-        }
+        let check = attend.firstChild.innerHTML
+        switchCheck(check, attend)
     }
 }
+
+function switchCheck(condition, item){
+    switch (Number(condition)){ 
+        case 1:
+            item.classList.toggle("attend_check1");
+            break;
+        case 2:
+            item.classList.toggle("attend_check2");
+            break;
+        case 3:
+            item.classList.toggle("attend_check3");
+            break;
+        default:
+            item.classList.toggle("attend_check0");
+    }
+}
+
+attends.forEach(function(e){
+    e.addEventListener('click', ()=>{
+        let check = e.firstChild.innerHTML
+        e.classList.remove(e.classList[1]);
+        if(check < 3){
+            check++
+            switchCheck(check, e)
+        } else{
+            check = 0
+            switchCheck(check, e)
+        }
+    })
+})
